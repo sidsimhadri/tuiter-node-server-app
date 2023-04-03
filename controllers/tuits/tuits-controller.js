3ee98e29156ec4d2936c6a2dce816688e23549db
sidsimhadri <sidsimhadri@gmail.com>
2023-04-02 22:51:06 -0400

    dislike

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -5,6 +5,8 @@ const createTuit = (req, res) => {
   const newTuit = req.body;
   newTuit._id = (new Date()).getTime()+'';
   newTuit.likes = 0;
+  newTuit.dislikes = 0;
+  newTuit.disliked = 0;
   newTuit.liked = false;
   tuits.push(newTuit);
   console.log(newTuit)
