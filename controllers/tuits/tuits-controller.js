1992b56bdde55f028240a3724ace4ab141f4b8f6
sidsimhadri <sidsimhadri@gmail.com>
2023-04-09 16:30:40 -0400

    dislikes again? i hate git sm

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -5,6 +5,7 @@ import * as tuitsDao from "./tuits-dao.js"
 const createTuit = async (req, res) => {
   const newTuit = req.body;
   newTuit.likes = 0;
+  newTuit.dislikes = 0;
   newTuit.liked = false;
   const insertedTuit = await tuitsDao
                              .createTuit(newTuit);
